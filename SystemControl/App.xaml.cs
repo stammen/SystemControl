22fbc148399c257d2dd076600a8419dcc1dda32a
Dale Stammen <dalestam@microsoft.com>
2018-04-30 10:08:38 -0700

    added copyright notice

--- SystemControl/App.xaml.cs
+++ SystemControl/App.xaml.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
--- SystemControlDesktopExtension/SystemControl.cpp
+++ SystemControlDesktopExtension/SystemControl.cpp
@@ -1,3 +1,13 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
 #include "SystemControl.h"
 #include "Brightness.h"
 #include <iostream>
